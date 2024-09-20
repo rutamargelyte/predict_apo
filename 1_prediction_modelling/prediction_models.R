735ef6208a68efa4afacd07ad15dcd86d1e58fa3
Ruta Margelyte <ruta.margelyte@bristol.ac.uk>
2024-09-20 16:22:34 +0100

    Create prediction_models.R

---
+++ 1_prediction_modelling/prediction_models.R
@@ -0,0 +1,16 @@
+
+#about##########################################################################
+
+#Multivariable logistic regression and RF/XGB prediction models 
+
+#setup##########################################################################
+
+
+#set location
+setwd("//ads.bris.ac.uk/Filestore/HealthSci SafeHaven/TOMMY/Ruta/code/Rmodels3")
+
+#clear environment/set print options
+rm(list=ls())
+options(max.print=99999)
+
+#load libraries#################################################################
